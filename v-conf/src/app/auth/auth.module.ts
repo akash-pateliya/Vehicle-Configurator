c3c755cb3249a7aa1a1724f46e4734923ee1dc75
Akash Pateliya <akash.pateliya@gmail.com>
2021-11-27 01:38:53 +0530

    auth added

---
+++ v-conf/src/app/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthComponent } from './auth.component';
+import { AdminSigninComponent } from './admin-signin/admin-signin.component';
+import { AdminSignupComponent } from './admin-signup/admin-signup.component';
+import { UserSignupComponent } from './user-signup/user-signup.component';
+import { UserSigninComponent } from './user-signin/user-signin.component';
+
+
+@NgModule({
+  declarations: [AuthComponent, AdminSigninComponent, AdminSignupComponent, UserSignupComponent, UserSigninComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
