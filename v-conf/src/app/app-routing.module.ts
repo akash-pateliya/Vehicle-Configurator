ca9c3949b72be7d38cf7f3bcc6f3274c6a82c1ba
Akash Pateliya <akash.pateliya@gmail.com>
2021-11-27 03:01:24 +0530

    look and feel added

--- v-conf/src/app/app-routing.module.ts
+++ v-conf/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }];
+const routes: Routes = [
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  {
+    path: '',
+    component: HomeComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
