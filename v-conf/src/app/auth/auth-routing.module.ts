6f76065696d5afd5336ec5e0222ab04c57a8f146
Akash Pateliya <akash.pateliya@gmail.com>
2021-11-27 04:27:43 +0530

    signin signup page added

--- v-conf/src/app/auth/auth-routing.module.ts
+++ v-conf/src/app/auth/auth-routing.module.ts
@@ -1,9 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminSigninComponent } from './admin-signin/admin-signin.component';
+import { AdminSignupComponent } from './admin-signup/admin-signup.component';
 
 import { AuthComponent } from './auth.component';
+import { UserSigninComponent } from './user-signin/user-signin.component';
+import { UserSignupComponent } from './user-signup/user-signup.component';
 
-const routes: Routes = [{ path: '', component: AuthComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent
+  },
+  {
+    path: 'user-signup',
+    component: UserSignupComponent
+  },
+  {
+    path: 'user-signin',
+    component: UserSigninComponent
+  },
+  {
+    path: 'admin-signup',
+    component: AdminSignupComponent
+  },
+  {
+    path: 'admin-signin',
+    component: AdminSigninComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- v-conf/src/app/auth/auth.module.ts
+++ v-conf/src/app/auth/auth.module.ts
@@ -5,12 +5,12 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { AdminSigninComponent } from './admin-signin/admin-signin.component';
 import { AdminSignupComponent } from './admin-signup/admin-signup.component';
-import { UserSignupComponent } from './user-signup/user-signup.component';
 import { UserSigninComponent } from './user-signin/user-signin.component';
+import { UserSignupComponent } from './user-signup/user-signup.component';
 
 
 @NgModule({
-  declarations: [AuthComponent, AdminSigninComponent, AdminSignupComponent, UserSignupComponent, UserSigninComponent],
+  declarations: [AuthComponent, AdminSigninComponent, AdminSignupComponent, UserSigninComponent, UserSignupComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
